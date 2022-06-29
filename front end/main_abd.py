01df50b9c678cee2e8a9669f91476a1b6c7fd3dd
MahmoudAAbdelkader <61502610+MahmoudAAbdelkader@users.noreply.github.com>
2022-06-29 17:56:57 +0200

    adding back buttons

--- front end/main_abd.py
+++ front end/main_abd.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from PyQt5.QtWidgets import *
 from password_strength import PasswordStats
@@ -6,9 +7,24 @@ from password_strength import PasswordPolicy
 from email_validator import validate_email, EmailNotValidError
 from pyisemail import is_email
 #from client import *
+from PyQt5.QtCore import *
+from pynotifier import Notification
+
 
 app = QApplication(sys.argv)
 widget = QtWidgets.QStackedWidget()
+
+#def isvalid(bdate):
+#    if(len(bdate) != 10):
+#        return 0
+#    if(bdate[0].isdigit() and bdate[1].isdigit()
+#            and bdate[2].isdigit() and bdate[3].isdigit() and bdate[5].isdigit()
+ #           and bdate[6].isdigit() and bdate[8].isdigit() and bdate[9].isdigit()
+  #          and (bdate[0] == 1 or bdate[0] == 2) and bdate[4] == '-'
+   #         and bdate[7] == '-' ):
+    #    return 1
+    #return 0
+
 
 def showerror(errorCause,details):
     msg = QMessageBox()
@@ -21,6 +37,15 @@ def showerror(errorCause,details):
     # msg.buttonClicked.connect(msgbtn)
     msg.exec_()
 
+def notify(title,desc,duration = 10,path='',urg = 'normal'):
+    Notification(
+        title=title,
+        description=desc,
+        icon_path=path,  # On Windows .ico is required, on Linux - .png  EX: '/absolute/path/to/image/icon.png'
+        duration=duration,  # Duration in seconds
+        urgency=urg
+    ).send()
+
 class Login(QMainWindow):
     def __init__(self):
         super(Login, self).__init__()
@@ -28,6 +53,7 @@ class Login(QMainWindow):
         self.loginbutton.clicked.connect(self.gotomainpage)
         self.passwordin.setEchoMode(QLineEdit.Password)
         self.donthavebutton.clicked.connect(self.gotocreateacc)
+        notify('Login Page','Login page is displayed')
 
     def gotocreateacc(self):
         createacc = createAcc()
@@ -62,9 +88,16 @@ class createAcc(QMainWindow): #back to login page
     def __init__(self):
         super(createAcc, self).__init__()
         uic.loadUi('create_account.ui', self)
+        self.backbutton.clicked.connect(self.goback)
         self.signupbutton.clicked.connect(self.gotomainpage)
         self.passin.setEchoMode(QLineEdit.Password)
         self.cpassin.setEchoMode(QLineEdit.Password)
+        self.bdatein.setDisplayFormat("yyyy-MM-dd")
+        self.bdatein.setMaximumDate(QDate(2015,12,30))
+        self.bdatein.setMinimumDate(QDate(1900, 1, 1))
+        #self.passinfo.setEnabled()
+        #self.passinfo.setMouseTracking()
+        #self.passinfo.mouseMoveEvent()
 
     def gotomainpage(self):
         policy = PasswordPolicy.from_names(
@@ -85,20 +118,20 @@ class createAcc(QMainWindow): #back to login page
         country = self.countryin.text()
         job = self.jobin.text()
 
-        if not is_email(email,allow_gtld=False):
+        if (not fname.isalpha() or not lname.isalpha()):
+            showerror("Invalid Name", "Name must be all letters")
+        elif not is_email(email,allow_gtld=False):
             showerror('Invalid Email','')
-        elif (not job.isalpha()):
-            showerror("Invalid Job", '')
-        elif not country.isalpha():
-            showerror("Invalid Country", '')
-        elif not city.isalpha():
-            showerror("Invalid City", '')
         elif (len(policy.test(password))!=0):
             showerror("Invalid Password", "Password length must be 10 min and at least contains 1 upper case letter 1 digit and 1 special char")
         elif (cpassword!=password):
             showerror("password doesn't match", "")
-        elif (not fname.isalpha() or not lname.isalpha()):
-            showerror("Invalid Name", "Name must be all letters")
+        elif not country.isalpha():
+            showerror("Invalid Country", '')
+        elif not city.isalpha():
+            showerror("Invalid City", '')
+        elif (not job.isalpha()):
+            showerror("Invalid Job", '')
         #elif(Socket.requestServer(CREATE_ACCOUNT, {EMAIL:email, PASSWORD:password, JOB:job, CITY:city,
          #                                          COUNTRY:country, LNAME:lname, FNAME:fname, BDATE:bdate})[RESPONSE] == NO):
           #  showerror("Email Exists Before", 'Try another Email')
@@ -107,12 +140,21 @@ class createAcc(QMainWindow): #back to login page
             widget.addWidget(mainpage)
             widget.setCurrentIndex(widget.currentIndex() + 1)
 
+    def goback(self):
+        widget.setCurrentIndex(widget.currentIndex() - 1)
+        dwidget = widget(widget.currentIndex() + 1)
+        widget.removeWidget(dwidget)
+
 class editAccount(QMainWindow): #back to view page
     def __init__(self):
         super(editAccount, self).__init__()
         uic.loadUi('edit_acc.ui', self)
         self.savebutton.clicked.connect(self.gotoviewacc)
         self.chpassbutton.clicked.connect(self.gotochangepass)
+        self.bdatein.setDisplayFormat("yyyy-MM-dd")
+        self.bdatein.setMaximumDate(QDate(2015, 12, 30))
+        self.bdatein.setMinimumDate(QDate(1900, 1, 1))
+
         #data = Socket.requestServer(GET_PROFILE)
         #self.mailview.setText(Socket.getEmail())
         #self.fnamein.setText(data[FNAME])
@@ -133,7 +175,6 @@ class editAccount(QMainWindow): #back to view page
 
         if (not fname.isalpha() or not lname.isalpha()):
             showerror("Invalid Name", "Name must be all letters")
-        ############################## Dont forget BDAAAAAAATEEEEEEEE
         elif not country.isalpha():
             showerror("Invalid Country", '')
         elif not city.isalpha():
@@ -182,7 +223,7 @@ class viewAccount(QMainWindow): #back button main page
         widget.addWidget(editaccount)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
-mainwindow = viewAccount()
+mainwindow = Login()
 widget.addWidget(mainwindow)
 widget.setFixedHeight(600)
 widget.setFixedWidth(800)
